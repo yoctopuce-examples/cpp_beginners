91a8296698441a9952d2ed40e35f00efeead7e6b
Yoctopuce Sarl <dev@yoctopuce.com>
2018-02-09 10:39:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,36 +6,36 @@
 
 using namespace std;
 
-
 int main(int argc, const char* argv[])
 {
-    string errmsg;
-    if (YAPI::RegisterHub("usb", errmsg) != YAPI::SUCCESS) {
-        cerr << "RegisterHub error: " << errmsg << endl;
-        return 1;
-    }
+  string errmsg;
+  if (YAPI::RegisterHub("usb", errmsg) != YAPI::SUCCESS) {
+    cerr << "RegisterHub error: " << errmsg << endl;
+    return 1;
+  }
 
-    YTemperature* t = YTemperature::FirstTemperature();
-    if (t == NULL) {
-        cerr << "No temperature sensor found" << endl;
-        return 1;
-    }
+  YTemperature* t = YTemperature::FirstTemperature();
+  if (t == NULL) {
+    cerr << "No temperature sensor found" << endl;
+    return 1;
+  }
 
-    YRelay* r = YRelay::FirstRelay();
-    if (r == NULL) {
-        cerr << "No relay found" << endl;
-        return 1;
-    }
+  YRelay* r = YRelay::FirstRelay();
+  if (r == NULL) {
+    cerr << "No relay found" << endl;
+    return 1;
+  }
 
-    while (true) {
-        double value = t->get_currentValue();
-        if (value > 25) {
-            r->set_state(YRelay::STATE_B);
-        }
-        if (value < 24) {
-            r->set_state(YRelay::STATE_A);
-        }
-        YAPI::Sleep(100, errmsg);
+  while (t->isOnline() && r->isOnline()) {
+    double value = t->get_currentValue();
+    if (value > 25) {
+      r->set_state(YRelay::STATE_B);
     }
-    return 0;
+    if (value < 24) {
+      r->set_state(YRelay::STATE_A);
+    }
+    YAPI::Sleep(100, errmsg);
+  }
+  YAPI::FreeAPI();
+  return 0;
 }
